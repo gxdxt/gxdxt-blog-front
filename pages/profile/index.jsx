28cb7df57d41898b3b567f6aa7450337704240b5
gxdxt <yxxsxxny@gmail.com>
2022-12-12 12:02:42 +0900

    design: change the header

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -41,10 +41,8 @@ const ProfilePage = ({ profileData }) => {
       return (
           <div className = "header">
               <div className = "header-logo-div">
-                  <img className = "header-logo" src={logo} onClick = {() => {
-                    window.location.href = "/"
-                  }}></img>
-              </div>
+              
+		gxdxt</div>
               <div className="theme-btn-div">
                 <a className="theme-btn" onClick={changeColor}><img src={theme} className="theme-btn-icon"></img></a>
               </div>
